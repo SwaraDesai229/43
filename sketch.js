06323902dc29829f454d31b0f29e4f59ff4ea926
SwaraDesai229 <96266788+SwaraDesai229@users.noreply.github.com>
2022-04-24 20:08:59 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+// you can store data inside containera(variables)using "var"
+var x=2;//number
+console.log(x);
+
+var color="red";//text or string
+console.log(color);
+console.log(color.length);
+console.log(color.toUpperCase());
+
+var ateFood=false;//booleans 
+console.log(ateFood);
+
+var age=null;//null
+console.log(age);
+
+var score;//undefined
+console.log(score);
+
+//Arithemetic Operations
+var a=2+3;
+console.log("2+3="+a);
+
+var a=2*3;
+console.log("2*3="+a);
